7ccf547ab0eab7af677cbe41988397d2811c3ca2
Adrien <adrien.caranta@hotmail.fr>
2010-07-02 17:42:51 +0200

    Ajout: Requète et Affiche DB
    Implémentation du AppDelegate pour l'initialisation de la base de donnée
    Récupération des données de BDD et Affiche dans ListViewController

--- Classes/ExoSQLiteAppDelegate.m
+++ Classes/ExoSQLiteAppDelegate.m
@@ -12,18 +12,79 @@
 
 @implementation ExoSQLiteAppDelegate
 
-@synthesize window;
+@synthesize window, dbFilePath;
 @synthesize navigationController;
+
+NSString *DATABASE_RESOURCE_NAME = @"hotel";
+NSString *DATABASE_RESOURCE_TYPE = @"db";
+NSString *DATABASE_FILE_NAME = @"hotel.db";
 
 
 #pragma mark -
 #pragma mark Application lifecycle
 
+- (BOOL) initializeDb {
+	
+    NSLog (@"Début initializeDB");
+    
+    // Va voir si il trouve bien le fichier DATABASE_FILE_NAME
+    
+	NSArray *searchPaths = NSSearchPathForDirectoriesInDomains (NSDocumentDirectory, NSUserDomainMask, YES);
+	NSString *documentFolderPath = [searchPaths objectAtIndex: 0];
+	
+    dbFilePath = [documentFolderPath stringByAppendingPathComponent: DATABASE_FILE_NAME];
+
+	[dbFilePath retain];
+    
+    // NSLog(@"dbFilePath: %@", dbFilePath); // Chemin d'accès OK 
+    
+	if (! [[NSFileManager defaultManager] fileExistsAtPath: dbFilePath]) {
+
+		NSString *backupDbPath = [[NSBundle mainBundle] pathForResource:DATABASE_RESOURCE_NAME ofType:DATABASE_RESOURCE_TYPE];
+        
+      //  NSLog(@"BackupDbPath: %@", backupDbPath); // OK il ne rentre pas dans le if
+		
+        if (backupDbPath == nil) {
+
+			return NO;
+            
+		} 
+        else {
+			BOOL copiedBackupDb = [[NSFileManager defaultManager] copyItemAtPath:backupDbPath toPath:dbFilePath error:nil];
+			
+            if (! copiedBackupDb) {
+
+				return NO;
+			}
+		}
+	}
+    
+    NSLog (@"Fin initializeDB");
+    
+	return YES;
+
+
+}
+
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
     // Override point for customization after app launch    
 	
+    if (! [self initializeDb]) {
+        
+        NSLog(@"DB pas initialisée");
+        
+    } else {
+        
+        NSLog(@"DB Initialisée");
+        
+    }
+
+    
+    
 	[window addSubview:[navigationController view]];
     [window makeKeyAndVisible];
+    
 	return YES;
 }
 
@@ -39,6 +100,7 @@
 - (void)dealloc {
 	[navigationController release];
 	[window release];
+    [dbFilePath release];
 	[super dealloc];
 }
 
